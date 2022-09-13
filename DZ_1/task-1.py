3f17635f15ca9c0545e29c0cdb7b796698500db6
iMac <89259058289@mail.ru>
2022-09-13 21:15:58 +0300

    DZ_1

---
+++ DZ_1/task-1.py
@@ -0,0 +1,19 @@
+ # Напишите программу, которая принимает на вход цифру, обозначающую день недели 
+ # и проверяет, является ли этот день выходным.
+
+number_day = int(input("Введите день недели: "))
+if number_day == 1: 
+   print("Будний день")
+elif number_day == 2:
+    print("Будний день")
+elif number_day == 3:
+    print("Будний день")
+elif number_day == 4:
+    print("Будний день")
+elif number_day == 5:
+    print("Будний день")
+elif number_day == 6 or number_day == 7:
+    print("Выходной день")
+else: 
+   print("Не является днем недели")
+
